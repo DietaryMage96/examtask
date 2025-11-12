a4d27495321b34172ca306e0317ece74a02e46fc
DietaryMage96 <ivasenkoartur02@gmail.com>
2025-11-12 19:46:02 +0200

    Реалізовано конструктори (звичайний, explicit та конструктор копіювання).

--- examtask/examtask.cpp
+++ examtask/examtask.cpp
@@ -9,7 +9,14 @@ private:
 	double price;
 	bool available;
 public:
-
+	Product(string n, int c, double p, bool a) : name(n), count(c), price(p), available(a) {};
+	explicit Product(string n) : name(n), count(0), price(0.0), available(false) {};
+	Product(Product& product) {
+		this->name = product.name;
+		this->count= product.count;
+		this->price = product.price;
+		this->available = product.available;
+	};
 };
 int main()
 {
